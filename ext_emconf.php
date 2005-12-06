575efec8292ba660f01ee3561e4f366877c6a5ea
Jeff Segars <jeff@webempoweredchurch.org>
2005-12-06 18:33:30 +0000

    Updated due to TYPO3.org upload.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "wec_flashpresentation"
 # 
-# Auto generated 02-12-2005 15:32
+# Auto generated 06-12-2005 12:33
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -13,25 +13,30 @@ $EM_CONF[$_EXTKEY] = Array (
 	'title' => 'WEC Flash Presentation',
 	'description' => 'Flash-based player for displaying synchronized media and presentation slides.',
 	'category' => 'plugin',
-	'author' => 'Web Empowered Church Team',
-	'author_email' => 'flashpresentation@webempoweredchurch.org',
-	'author_company' => 'Foundation For Evangelism',
-	'shy' => '',
+	'shy' => 0,
 	'dependencies' => 'cms',
 	'conflicts' => '',
 	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '-',
+	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
+	'internal' => 0,
 	'uploadfolder' => 1,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'private' => '',
+	'author' => 'Web Empowered Church Team',
+	'author_email' => 'flashpresentation@webempoweredchurch.org',
+	'author_company' => 'Foundation For Evangelism',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'private' => 0,
 	'download_password' => '',
-	'version' => '1.0.1',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:28:{s:33:"class.tx_wecflashpresentation.php";s:4:"e631";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"d241";s:28:"ext_typoscript_constants.txt";s:4:"e5fe";s:24:"ext_typoscript_setup.txt";s:4:"03f8";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"5e7f";s:21:"flexform_ds_video.xml";s:4:"defd";s:13:"locallang.php";s:4:"30ee";s:17:"locallang_csh.php";s:4:"a38f";s:16:"locallang_db.php";s:4:"b7e8";s:28:"wec_flashpresentation.tmproj";s:4:"a604";s:17:"wizard_slides.php";s:4:"1737";s:14:"doc/manual.sxw";s:4:"9537";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"d451";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"c5ae";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"1fd4";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"4341";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:17:"res/mp3_chart.swf";s:4:"a7e0";s:19:"res/video_chart.swf";s:4:"3c88";}',
+	'version' => '1.1.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:28:{s:33:"class.tx_wecflashpresentation.php";s:4:"5fbc";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"d241";s:28:"ext_typoscript_constants.txt";s:4:"e5fe";s:24:"ext_typoscript_setup.txt";s:4:"03f8";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"ba8e";s:21:"flexform_ds_video.xml";s:4:"d975";s:13:"locallang.php";s:4:"30ee";s:17:"locallang_csh.php";s:4:"a38f";s:16:"locallang_db.php";s:4:"b7e8";s:28:"wec_flashpresentation.tmproj";s:4:"a604";s:17:"wizard_slides.php";s:4:"1737";s:14:"doc/manual.sxw";s:4:"82fa";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"d451";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"c5ae";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"1fd4";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"4341";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:17:"res/mp3_chart.swf";s:4:"a7e0";s:19:"res/video_chart.swf";s:4:"8576";}',
 );
 
 ?>
