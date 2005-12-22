c5ca44964e1fb949d35ba0dad07a5ec0da2898bb
Jeff Segars <jeff@webempoweredchurch.org>
2005-12-22 16:07:19 +0000

    Updated ext_emconf.php and manual to reflect latest changes.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "wec_flashpresentation"
 # 
-# Auto generated 06-12-2005 12:33
+# Auto generated 22-12-2005 10:07
 # 
 # Manual updates:
 # Only the data in the array - anything else is removed by next write
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = Array (
 	'private' => 0,
 	'download_password' => '',
 	'version' => '1.1.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:28:{s:33:"class.tx_wecflashpresentation.php";s:4:"5fbc";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"d241";s:28:"ext_typoscript_constants.txt";s:4:"e5fe";s:24:"ext_typoscript_setup.txt";s:4:"03f8";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"ba8e";s:21:"flexform_ds_video.xml";s:4:"d975";s:13:"locallang.php";s:4:"30ee";s:17:"locallang_csh.php";s:4:"a38f";s:16:"locallang_db.php";s:4:"b7e8";s:28:"wec_flashpresentation.tmproj";s:4:"a604";s:17:"wizard_slides.php";s:4:"1737";s:14:"doc/manual.sxw";s:4:"82fa";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"d451";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"c5ae";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"1fd4";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"4341";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:17:"res/mp3_chart.swf";s:4:"a7e0";s:19:"res/video_chart.swf";s:4:"8576";}',
+	'_md5_values_when_last_written' => 'a:29:{s:33:"class.tx_wecflashpresentation.php";s:4:"5fbc";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"d241";s:28:"ext_typoscript_constants.txt";s:4:"e5fe";s:24:"ext_typoscript_setup.txt";s:4:"03f8";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"ba8e";s:21:"flexform_ds_video.xml";s:4:"d975";s:13:"locallang.php";s:4:"30ee";s:17:"locallang_csh.php";s:4:"a38f";s:16:"locallang_db.php";s:4:"b7e8";s:28:"wec_flashpresentation.tmproj";s:4:"a604";s:17:"wizard_slides.php";s:4:"1737";s:14:"doc/manual.sxw";s:4:"1cf4";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"d451";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"c5ae";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"1fd4";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"4341";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:17:"res/mp3_chart.swf";s:4:"c451";s:13:"res/test.html";s:4:"31c0";s:19:"res/video_chart.swf";s:4:"8576";}',
 );
 
 ?>
