544c103be7ce1dbaa7424c17eddc7485bd87b11a
Jeff Segars <jeff@webempoweredchurch.org>
2006-05-25 18:17:47 +0000

    Updated ext_emconf for new files.

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,15 @@
 
 ########################################################################
 # Extension Manager/Repository config file for ext: "wec_flashpresentation"
-# 
-# Auto generated 22-12-2005 10:07
-# 
+#
+# Auto generated 25-05-2006 13:16
+#
 # Manual updates:
-# Only the data in the array - anything else is removed by next write
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = Array (
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'WEC Flash Presentation',
 	'description' => 'Flash-based player for displaying synchronized media and presentation slides.',
 	'category' => 'plugin',
@@ -18,8 +19,6 @@ $EM_CONF[$_EXTKEY] = Array (
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '-',
-	'PHP_version' => '-',
 	'module' => '',
 	'state' => 'stable',
 	'internal' => 0,
@@ -28,15 +27,26 @@ $EM_CONF[$_EXTKEY] = Array (
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'author' => 'Web-Empowered Church Team',
+	'author' => 'Web Empowered Church Team',
 	'author_email' => 'flashpresentation@webempoweredchurch.org',
 	'author_company' => 'Foundation For Evangelism',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'private' => 0,
-	'download_password' => '',
-	'version' => '1.1.0',	// Don't modify this! Managed automatically during upload to repository.
-	'_md5_values_when_last_written' => 'a:29:{s:33:"class.tx_wecflashpresentation.php";s:4:"5fbc";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"d241";s:28:"ext_typoscript_constants.txt";s:4:"e5fe";s:24:"ext_typoscript_setup.txt";s:4:"03f8";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"ba8e";s:21:"flexform_ds_video.xml";s:4:"d975";s:13:"locallang.php";s:4:"30ee";s:17:"locallang_csh.php";s:4:"a38f";s:16:"locallang_db.php";s:4:"b7e8";s:28:"wec_flashpresentation.tmproj";s:4:"a604";s:17:"wizard_slides.php";s:4:"1737";s:14:"doc/manual.sxw";s:4:"1cf4";s:19:"doc/wizard_form.dat";s:4:"e623";s:20:"doc/wizard_form.html";s:4:"3246";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"d451";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"c5ae";s:17:"pi1/locallang.php";s:4:"d41d";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"1fd4";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"4341";s:17:"pi2/locallang.php";s:4:"d41d";s:12:"res/100k.jpg";s:4:"e9cc";s:17:"res/mp3_chart.swf";s:4:"c451";s:13:"res/test.html";s:4:"31c0";s:19:"res/video_chart.swf";s:4:"8576";}',
+	'version' => '1.1.0',
+	'_md5_values_when_last_written' => 'a:27:{s:33:"class.tx_wecflashpresentation.php";s:4:"0071";s:45:"class.tx_wecflashpresentation_flashobject.php";s:4:"be08";s:12:"ext_icon.gif";s:4:"f772";s:17:"ext_localconf.php";s:4:"d6d7";s:14:"ext_tables.php";s:4:"d241";s:28:"ext_typoscript_constants.txt";s:4:"e5fe";s:24:"ext_typoscript_setup.txt";s:4:"03f8";s:24:"flash-ext_icon-sound.gif";s:4:"d476";s:21:"flexform_ds_audio.xml";s:4:"ba8e";s:21:"flexform_ds_video.xml";s:4:"d975";s:13:"locallang.xml";s:4:"bede";s:16:"locallang_db.xml";s:4:"ae5c";s:28:"wec_flashpresentation.tmproj";s:4:"a604";s:17:"wizard_slides.php";s:4:"1737";s:14:"doc/manual.sxw";s:4:"adfb";s:14:"pi1/ce_wiz.gif";s:4:"43fe";s:41:"pi1/class.tx_wecflashpresentation_pi1.php";s:4:"1e80";s:49:"pi1/class.tx_wecflashpresentation_pi1_wizicon.php";s:4:"7545";s:17:"pi1/locallang.xml";s:4:"cede";s:14:"pi2/ce_wiz.gif";s:4:"9d69";s:41:"pi2/class.tx_wecflashpresentation_pi2.php";s:4:"b250";s:49:"pi2/class.tx_wecflashpresentation_pi2_wizicon.php";s:4:"d14a";s:17:"pi2/locallang.xml";s:4:"5d0b";s:12:"res/100k.jpg";s:4:"e9cc";s:18:"res/flashobject.js";s:4:"9568";s:17:"res/mp3_chart.swf";s:4:"c451";s:19:"res/video_chart.swf";s:4:"c4a4";}',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+			'php' => '3.0.0-',
+			'typo3' => '3.5.0-',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
 );
 
 ?>
