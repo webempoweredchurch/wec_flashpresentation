b0e34ad61add0ac379d6628e6a0b876f4502ce2e
Jeff Segars <jeff@webempoweredchurch.org>
2006-07-31 20:37:15 +0000

    Updated FlashObject JS to new SWFObject naming.

--- class.tx_wecflashpresentation_flashobject.php
+++ class.tx_wecflashpresentation_flashobject.php
@@ -90,7 +90,7 @@ class tx_wecflashpresentation_flashobject {
 	 */
 	function output() {
 		
-		$GLOBALS['TSFE']->additionalHeaderData[] = '<script type="text/javascript" src="'.$this->flashObjectPath.'flashobject.js"></script>';		
+		$GLOBALS['TSFE']->additionalHeaderData[] = '<script type="text/javascript" src="'.$this->flashObjectPath.'swfobject.js"></script>';		
 				
 		$output .= $this->flashObject.chr(10);
 		$output .= implode(chr(10), $this->flashObjectVariables).chr(10);
