3909a53dbceb41470759acc2cb02307ebfbc584f
Jeff Segars <jeff@webempoweredchurch.org>
2006-08-01 16:11:55 +0000

    Added missing include for FlashObject.

--- class.tx_wecflashpresentation.php
+++ class.tx_wecflashpresentation.php
@@ -34,6 +34,8 @@
 
 
 require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once('class.tx_wecflashpresentation_flashobject.php');
+
 
 /** 
  * Top level class for the 'wec_flashpresentation' extension. Subclasses
